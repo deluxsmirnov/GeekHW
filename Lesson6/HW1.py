d43a1774bf1eed402dad93a8845f80d6f6b08f38
Serezha Smirnov <serezha-smirnov@mail.ru>
2020-03-09 21:22:32 +0500

    Сделал сфетофор ДЗ1

---
+++ Lesson6/HW1.py
@@ -0,0 +1,22 @@
+from time import sleep
+from termcolor import colored
+
+class TrafficLight:
+    __color = ''
+    def traffic_light(self):
+        i = 0
+        while i < 80:
+            print(colored('Загорелся красный цвет сфетофора', 'red'))
+            sleep(7)
+            print(colored('Загорелся желтый цвет сфетофора', 'yellow'))
+            sleep(2)
+            print(colored('Загорелся зеленый цвет сфетофора', 'green'))
+            sleep(7)
+            print(colored('Загорелся желтый цвет сфетофора', 'yellow'))
+            sleep(2)
+            a += 1
+
+go = TrafficLight()
+
+go.traffic_light()
+
